4476f0dc31317e47be0d8c269cd54e409ee897b0
Robert Anker <robert@masys.co.uk>
2021-01-28 13:38:31 +0000

    When iterating through subscriptions to get membership type, instead of iterating through them all, it now just iterates through the subscriptions for this membership package. This fixed the issue of type and status not being displayed, because the body of the else statement was being ran, and made type and status empty string; this now doesn't happen.

--- membership/views.py
+++ membership/views.py
@@ -552,7 +552,7 @@ def get_members(request, title):
     if all_members.count() > 0:
         for member in all_members:
             # get membership type
-            for sub in member.subscription.all():
+            for sub in member.subscription.filter(membership_package=membership_package):
                 if sub.membership_package == membership_package:
                     try:
                         membership_type = f"""<span class="badge py-1 badge-info">{sub.price.nickname}</span>"""
