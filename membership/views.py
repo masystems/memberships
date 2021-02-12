7b5a4a83dd68af1a6f088d8e727a2c9bcbe26c99
Robert Anker <robert@masys.co.uk>
2021-02-12 14:40:36 +0000

    Replaced \n with <br/> in the email body so that new lines are created in the email.

--- membership/views.py
+++ membership/views.py
@@ -1072,6 +1072,9 @@ def payment_reminder(request, title, pk):
     else:
         body = membership_package.payment_reminder_email
         customised = True
+
+        # add in the new lines
+        body = body.replace('\n', '<br/>')
     
     send_email(f"Payment Reminder: {membership_package.organisation_name}", request.user.get_full_name(), body, send_to=member.user_account.email, customised=customised)
 
