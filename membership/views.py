16082812a101873a81ab112afd3f2cfc59f24618
Robert Anker <robert@masys.co.uk>
2021-02-18 15:05:42 +0000

    Check subscription is active

--- membership/views.py
+++ membership/views.py
@@ -997,74 +997,76 @@ def reports(request, title, report, file_type):
         # get rows
         subscriptions = MembershipSubscription.objects.filter(membership_package=membership_package)
         for subscription in subscriptions:
-            # get custom fields
-            mail = True
-            second_name = ''
-            raffle_tickets = 'Yes'
-            custom_fields = loads(subscription.custom_fields)
+            # check they are active
+            if subscription.active:
+                # get custom fields
+                mail = True
+                second_name = ''
+                raffle_tickets = 'Yes'
+                custom_fields = loads(subscription.custom_fields)
 
-            for key, field in custom_fields.items():
-                # do not mail
-                if field['field_name'] == 'Do not mail':
-                    try:
-                        # do not mail is true, so don't mail
-                        if field['field_value']:
-                            mail = False
-                            break
-                    # they haven't ever ticked the box, so mail
-                    except KeyError:
-                        pass
-                # second name
-                elif field['field_name'] == 'Second name':
-                    try:
-                        second_name = field['field_value']
-                    except KeyError:
-                        pass
-                # raffle tickets
-                elif field['field_name'] == 'No raffle tickets':
-                    try:
-                        # no raffle tickets is true, so no
-                        if field['field_value']:
-                            raffle_tickets = 'No'
-                    # they haven't ever ticked the box, so yes
-                    except KeyError:
-                        pass
-            
-            # try to get membership type
-            try:
-                membership_type = subscription.price.nickname
-            except AttributeError:
-                membership_type = ''
-            
-            # add subscription data to table if mail is True
-            if mail:
-                row_num = row_num + 1
-                # title
-                worksheet.write(row_num, 0, subscription.member.title, font_style)
-                # first name
-                worksheet.write(row_num, 1, subscription.member.user_account.first_name, font_style)
-                # surname
-                worksheet.write(row_num, 2, subscription.member.user_account.last_name, font_style)
-                # second name
-                worksheet.write(row_num, 3, second_name, font_style)
-                # membership type
-                worksheet.write(row_num, 4, membership_type, font_style)
-                # company name
-                worksheet.write(row_num, 5, subscription.member.company, font_style)
-                # address 1
-                worksheet.write(row_num, 6, subscription.member.address_line_1, font_style)
-                # address 2
-                worksheet.write(row_num, 7, subscription.member.address_line_2, font_style)
-                # address 3 (town)
-                worksheet.write(row_num, 8, subscription.member.town, font_style)
-                # address 4 (county)
-                worksheet.write(row_num, 9, subscription.member.county, font_style)
-                # postcode
-                worksheet.write(row_num, 10, subscription.member.postcode, font_style)
-                # country
-                worksheet.write(row_num, 11, subscription.member.country, font_style)
-                # raffle tickets
-                worksheet.write(row_num, 12, raffle_tickets, font_style)
+                for key, field in custom_fields.items():
+                    # do not mail
+                    if field['field_name'] == 'Do not mail':
+                        try:
+                            # do not mail is true, so don't mail
+                            if field['field_value']:
+                                mail = False
+                                break
+                        # they haven't ever ticked the box, so mail
+                        except KeyError:
+                            pass
+                    # second name
+                    elif field['field_name'] == 'Second name':
+                        try:
+                            second_name = field['field_value']
+                        except KeyError:
+                            pass
+                    # raffle tickets
+                    elif field['field_name'] == 'No raffle tickets':
+                        try:
+                            # no raffle tickets is true, so no
+                            if field['field_value']:
+                                raffle_tickets = 'No'
+                        # they haven't ever ticked the box, so yes
+                        except KeyError:
+                            pass
+                
+                # try to get membership type
+                try:
+                    membership_type = subscription.price.nickname
+                except AttributeError:
+                    membership_type = ''
+                
+                # add subscription data to table if mail is True
+                if mail:
+                    row_num = row_num + 1
+                    # title
+                    worksheet.write(row_num, 0, subscription.member.title, font_style)
+                    # first name
+                    worksheet.write(row_num, 1, subscription.member.user_account.first_name, font_style)
+                    # surname
+                    worksheet.write(row_num, 2, subscription.member.user_account.last_name, font_style)
+                    # second name
+                    worksheet.write(row_num, 3, second_name, font_style)
+                    # membership type
+                    worksheet.write(row_num, 4, membership_type, font_style)
+                    # company name
+                    worksheet.write(row_num, 5, subscription.member.company, font_style)
+                    # address 1
+                    worksheet.write(row_num, 6, subscription.member.address_line_1, font_style)
+                    # address 2
+                    worksheet.write(row_num, 7, subscription.member.address_line_2, font_style)
+                    # address 3 (town)
+                    worksheet.write(row_num, 8, subscription.member.town, font_style)
+                    # address 4 (county)
+                    worksheet.write(row_num, 9, subscription.member.county, font_style)
+                    # postcode
+                    worksheet.write(row_num, 10, subscription.member.postcode, font_style)
+                    # country
+                    worksheet.write(row_num, 11, subscription.member.country, font_style)
+                    # raffle tickets
+                    worksheet.write(row_num, 12, raffle_tickets, font_style)
 
         workbook.save(response)
         return response
