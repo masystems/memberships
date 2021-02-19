79e56fd6c4ec8a546768b1c87fd3f7f9808fabb6
masystems <marco@masys.co.uk>
2021-02-19 09:09:10 +0000

    updated to handle xls files

--- membership/views.py
+++ membership/views.py
@@ -967,8 +967,8 @@ def reports(request, title, report, file_type):
     membership_package = MembershipPackage.objects.get(organisation_name=title)
     date = datetime.now()
     if file_type == 'xlsx':
-        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8')
-        response['Content-Disposition'] = f'attachment; filename="{membership_package}-Export-{date.strftime("%Y-%m-%d")}.xlsx"'
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = f'attachment; filename="{membership_package}-Export-{date.strftime("%Y-%m-%d")}.xls"'
         
         # creating workbook
         workbook = xlwt.Workbook(encoding='utf-8')
