33d5fb31019531fd27711dfd3b8f7a8b20661942
Robert Anker <robert@masys.co.uk>
2021-02-22 16:04:43 +0000

    Converted amount into pounds before it is displayed to user in the form.

--- membership/views.py
+++ membership/views.py
@@ -2001,8 +2001,13 @@ def member_payment_form_edit(request, title, pk, payment_id):
 
             return redirect('member_payments', membership_package.organisation_name, member.id)
     else:
-        form = PaymentForm(instance=payment)
+        # create a variable to store amount in pounds
+        payment_for_form = payment
+        payment_for_form.amount = float(int(payment.amount) / 100)
+        payment_for_form.amount = "%.2f" % payment_for_form.amount
 
+        form = PaymentForm(instance=payment_for_form)
+        
     return render(request, 'payment_form_edit.html', {'form': form,
                                                       'membership_package': membership_package,
                                                       'member': member,
