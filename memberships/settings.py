54e5bfcba55afe8ec568cd8ca740aa70cc1ec031
masystems <marco@masys.co.uk>
2021-03-18 09:15:27 +0000

    added logging level

--- memberships/settings.py
+++ memberships/settings.py
@@ -55,6 +55,20 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 WSGI_APPLICATION = 'memberships.wsgi.application'
 
 
